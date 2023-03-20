e10d082009a111aaab4cc9ec14e60253c09b559c
Realf <stefan.hodgman@gmail.com>
2023-03-20 18:00:29 +0200

    Asks you to select a workspace

--- DashboardGenerator.py
+++ DashboardGenerator.py
@@ -49,34 +49,23 @@ workspaces = root.findall(".//workspaceVO")
 for i, workspace in enumerate(workspaces):
     print(f"{i+1}. {workspace.find('Workspace_Name').text}")
 
-selected_workspace = None
-for workspace in workspaces:
-    workspace_name = workspace.find('Workspace_Name').text
-    if "Development" in workspace_name:
-        selected_workspace = workspace
-        break
-
-if selected_workspace is None:
-    print("No workspace with the word 'Development' found.")
-    workspace_num = int(input("Select a workspace by number: "))
-    selected_workspace = workspaces[workspace_num-1]
-
+workspace_num = int(input("Select a workspace by number: "))
+selected_workspace = workspaces[workspace_num-1]
 workspace_id = selected_workspace.find("Workspace_Id").text
 workspace_uris = selected_workspace.findall("URI")
 
-# # create a numbered list of workspace URIs
-# uri_list = []
-# for i, uri in enumerate(workspace_uris):
-#     uri_str = uri.text
-#     uri_name = uri_str.split("/")[-1]
-#     if uri_name in ["formtypelist", "getWorkspaceUsers", "createFormMsg", "getProjectCustomAttributeSet", "getProjectCustomAttributes", "saveCustomAttributeSet", "saveCustomAttributes", "getWorkspaceroles"]:
-#         uri_list.append(uri_str)
-#         print(f"{len(uri_list)}. {uri_name}")
+# create a numbered list of workspace URIs
+uri_list = []
+for i, uri in enumerate(workspace_uris):
+    uri_str = uri.text
+    uri_name = uri_str.split("/")[-1]
+    if uri_name in ["formtypelist", "getWorkspaceUsers", "createFormMsg", "getProjectCustomAttributeSet", "getProjectCustomAttributes", "saveCustomAttributeSet", "saveCustomAttributes", "getWorkspaceroles"]:
+        uri_list.append(uri_str)
+        print(f"{len(uri_list)}. {uri_name}")
 
-# # ask the user to select a URI by number
-# uri_num = int(input("Select a URI by number: "))
-# selected_uri = uri_list[uri_num-1]
+workspace_uris = selected_workspace.findall("URI")
 
+# Find the 'formtypelist' URI
 selected_uri = None
 for uri in workspace_uris:
     uri_str = uri.text
@@ -87,25 +76,8 @@ for uri in workspace_uris:
 
 if selected_uri is None:
     print("No 'formtypelist' URI found in the workspace URIs.")
-    # create a numbered list of workspace URIs
-    uri_list = []
-    for i, uri in enumerate(workspace_uris):
-        uri_str = uri.text
-        uri_name = uri_str.split("/")[-1]
-        if uri_name in ["formtypelist", "getWorkspaceUsers", "createFormMsg", "getProjectCustomAttributeSet", "getProjectCustomAttributes", "saveCustomAttributeSet", "saveCustomAttributes", "getWorkspaceroles"]:
-            uri_list.append(uri_str)
-            print(f"{len(uri_list)}. {uri_name}")
-    
-    # ask the user to select a URI by number
-    uri_num = int(input("Select a URI by number: "))
-    selected_uri = uri_list[uri_num-1]
+    sys.exit()
 
-
-# save the selected_uri to a file
-with open("SelectedURI.XML", "w") as f:
-    f.write(selected_uri)
-
-# (The rest of the main.py file remains the same)
 
 # save the curl_cmd to a file
 with open("curl_cmd.XML", "w") as f:
@@ -117,6 +89,7 @@ if selected_uri.endswith("formtypelist"):
 
     # Use the app_builder_id to download the form template
     get_FormTemplate(session_id, workspace_id, app_builder_id)
+
 
 # Check for user confirmation before calling createFormMsg
 if __name__ == "__main__":
